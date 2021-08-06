255949faeae6cbc5447fa46d3dfb797312eae69f
Jeroen Dalsem <jdalsem@coldtrick.com>
2021-08-06 10:16:47 +0200

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file was created by Translation Editor v8.0
+ * On 2021-08-06 10:16
+ */
+
+return array (
+  'item:object:saml_idp' => 'SAML IDP configuratie',
+  'admin:configure_utilities:manage_idps' => 'Beheer SAML IDPs',
+  'saml_sso:settings:use_http_x_forwarded' => 'Gebruik proxy variabelen',
+  'saml_sso:settings:use_http_x_forwarded:help' => 'Sta het gebruik toe van de HTTP_X_FORWARDED server informatie',
+  'add:object:saml_idp' => 'Maak IDP',
+  'add:object:saml_idp:from_xml' => 'Maak IDP obv XML',
+  'saml_sso:add_from_xml:url' => 'Voer IDP metadata URL in voor autodetectie',
+  'saml_sso:add_from_xml:xml' => 'of plak de IDP metadata XML',
+  'saml_sso:saml_idp:sso_url' => 'Signle Sign On URL',
+  'saml_sso:saml_idp:slo_url' => 'Single Log Out URL',
+  'saml_sso:saml_idp:x509cert' => 'x509 certificaat',
+  'saml_sso:saml_idp:private_key' => 'Private Key',
+  'saml_sso:saml_idp:show_on_login_form' => 'Toon op aanmeld formulier',
+  'saml_sso:force_authentication' => 'Geforceerde Authenticatie',
+  'saml_sso:force_authentication:enable' => 'Activeer Geforceerde Authenticatie',
+  'saml_sso:force_authentication:disable' => 'Deactiveer Geforceerde Authenticatie',
+);
